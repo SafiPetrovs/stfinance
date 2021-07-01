c10c92e08f001a3b2e7bd5ff306abf6c470e1c11
SafiPetrovs <86779020+SafiPetrovs@users.noreply.github.com>
2021-07-01 15:31:03 +0300

    Update App.js

--- petrovs/src/App.js
+++ petrovs/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 class="text">Stfinance website is under construction!</h1>
+      <h1 class="text">S.T Finance&Consultancy Ltd. website is under construction!</h1>
       <img class="image" src="https://cdn.discordapp.com/attachments/516262146095448085/859830384773824552/image0.png" alt="Site is in development"/>
     </div>
   );
